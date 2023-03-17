3f4ce1b1e0bfcfc65b5d5f158bbe2a719c3afae8
Jacob Engel <jacob.engel.91@gmail.com>
2023-03-17 18:36:37 -0400

    Lol.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 sudo /home/ec2-user/.dotnet/dotnet restore /var/www/MenuPlanner.WebHost/MenuPlanner.WebHost.csproj
 
-sudo /home/ec2-user/.dotnet/dotnet publish /var/www.MenuPlanner.WebHost/MenuPlanner.WebHost.csproj --configuration release -o /var/www/app/ --runtime linux-x64
+sudo /home/ec2-user/.dotnet/dotnet publish /var/www/MenuPlanner.WebHost/MenuPlanner.WebHost.csproj --configuration release -o /var/www/app/ --runtime linux-x64
 
 systemctl enable website.service
 systemctl start website.service
