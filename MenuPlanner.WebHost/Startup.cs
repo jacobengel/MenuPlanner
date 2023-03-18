685f0308a296dc83709d2c2ae5a2af6171e54864
Jacob Engel <jacob.engel.91@gmail.com>
2023-03-17 20:36:24 -0400

    Adding controllers.

--- MenuPlanner.WebHost/Startup.cs
+++ MenuPlanner.WebHost/Startup.cs
@@ -13,6 +13,7 @@
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,9 +24,11 @@
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                endpoints.MapControllers();
             });
         }
     }
